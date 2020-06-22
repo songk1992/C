e28796a03635664960ff648b6cc6fc938e94f8f8
laigasus <okjaeook98@gmail.com>
2020-06-22 10:35:58 +0000

    005 조건문 개념 배우기(if~else)

---
+++ 초보자를 위한 C언어 300제(원본)/005.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main(void) 
+{ 
+	int x;
+	int y;
+
+	x = 20;
+	y = 10;
+	
+	if( x > y )
+	{
+		printf( "x의 값이 y보다 큽니다." );
+	}
+	else
+	{
+		printf( "x의 값이 y보다 작거나 같습니다.");
+	}
+} 
