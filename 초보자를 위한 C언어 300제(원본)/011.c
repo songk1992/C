cb7b433b96c30c76697d3fac83609f15264aa7f9
laigasus <okjaeook98@gmail.com>
2020-06-22 10:36:37 +0000

    011 문자형 변수 이해하기(char)

---
+++ 초보자를 위한 C언어 300제(원본)/011.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(void) 
+{ 
+	char ch;
+	unsigned char j;
+	char k;
+
+	ch = 200;
+	j = 200;
+	k = 'a';
+
+	printf( "문자형 변수 i의 값은 %d \n", ch );
+	printf( "문자형 변수 j의 값은 %u \n", j );
+	printf( "문자형 변수 k의 값은 %d \n", k );
+} 
