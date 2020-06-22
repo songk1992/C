76a50003aa5f16a11d08e10582bf496815e75185
laigasus <okjaeook98@gmail.com>
2020-06-22 10:37:25 +0000

    020 문자열형 상수 이해하기(char)

---
+++ 초보자를 위한 C언어 300제(원본)/020.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+#define KOREA	"대한민국"
+#define BOOK	"This is a book."
+
+const char* SOCCOR = "나는 축구를 좋아합니다.";
+
+int main(void) 
+{ 
+	printf( "문자열형 상수  KOREA의 값 : %s \n", KOREA );
+	printf( "문자열형 상수   BOOK의 값 : %s \n", BOOK );
+	printf( "문자열형 상수 SOCCOR의 값 : %s \n", SOCCOR );
+} 
