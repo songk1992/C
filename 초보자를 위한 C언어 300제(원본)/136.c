5725892fd96ba2e8bde208d0e68d11f5de2c8fed
laigasus <okjaeook98@gmail.com>
2020-06-22 10:47:37 +0000

    136 파일 생성하기(fopen)

---
+++ 초보자를 위한 C언어 300제(원본)/136.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main( void )
+{
+	FILE *file;
+
+	file = fopen( "c:\\file.txt", "w+" );
+
+	if( file == NULL )
+	{
+		puts( "파일을 생성할 수 없습니다." );
+	}
+	else
+	{
+		puts( "파일이 정상적으로 생성되었습니다." );
+		fclose( file );
+	}
+}
+
