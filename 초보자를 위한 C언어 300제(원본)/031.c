61cbfabf645a5f7c4ea671786833989f815c29af
laigasus <okjaeook98@gmail.com>
2020-06-22 10:38:52 +0000

    031 sizeof 연산자 이해하기

---
+++ 초보자를 위한 C언어 300제(원본)/031.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main(void) 
+{
+	char i;
+	int  j;
+	double k;
+
+	printf( "%d \n", sizeof(i) );		// 1
+	printf( "%d \n", sizeof(j) );		// 4
+	printf( "%d \n", sizeof(k) );		// 8
+} 
+
